452faa75c6144d2198aa835a49495131848c2c87
Bastien Riviere <me@babariviere.com>
2022-05-12 22:16:09 +0200

    tailscal: add aws sdk

--- baba/packages/tailscale.scm
+++ baba/packages/tailscale.scm
@@ -30,3 +30,31 @@ package, and for encoding P12/PFX files for use by legacy applications which do
 not support newer formats.  Since PKCS#12 uses weak encryption primitives, it
 SHOULD NOT be used for new applications.")
     (license license:bsd-3)))
+
+(define-public go-github-com-aws-aws-sdk-go-v2-service-sts
+  (package
+    (name "go-github-com-aws-aws-sdk-go-v2-service-sts")
+    (version "1.16.3")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://github.com/aws/aws-sdk-go-v2")
+               (commit (string-append "v" version))))
+        (file-name (git-file-name name version))
+        (sha256
+          (base32 "1090xmqmryavl9j9p0qbv7n8lbhd7mpg56v2qskcn9cqhnz0bk7f"))))
+    (build-system go-build-system)
+    (arguments
+      '(#:import-path
+        "github.com/aws/aws-sdk-go-v2/service/sts"
+        #:unpack-path
+        "github.com/aws/aws-sdk-go-v2"))
+    (propagated-inputs
+      `(("go-github-com-aws-smithy-go" ,go-github-com-aws-smithy-go)))
+    (home-page "https://github.com/aws/aws-sdk-go-v2")
+    (synopsis #f)
+    (description
+      "Package sts provides the API client, operations, and parameter types for AWS
+Security Token Service.")
+    (license license:asl2.0)))
